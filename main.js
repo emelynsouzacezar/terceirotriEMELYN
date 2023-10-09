f2fb4678bfb1b11fa18e3ce6a657942302f3b1a1
emelynsouzacezar <128257714+emelynsouzacezar@users.noreply.github.com>
2023-10-08 21:42:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas=document.querySelectorAll('.tecla');
+
+let contador = 0;
+//Estrutura de repetição - Enquanto
+while(contador <listaDeTeclas.leigth){
+   listaDeTeclas[contador].onclick = function(){
+tocaSom('#som_tecla_splash');
+   };
+contador = contador + 1;
+}
