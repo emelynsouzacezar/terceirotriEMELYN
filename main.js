481c135465e44d1e54d5daafec1a433957071f5a
emelynsouzacezar <128257714+emelynsouzacezar@users.noreply.github.com>
2023-10-22 22:10:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,22 @@ function tocaSom(idElementoAudio){
 }
 const listaDeTeclas=document.querySelectorAll('.tecla');
 
-let contador = 0;
+
 //Estrutura de repetição - Enquanto
-while(contador <listaDeTeclas.leigth){
-   listaDeTeclas[contador].onclick = function(){
-tocaSom('#som_tecla_splash');
+for( let contador = 0;  contador < listaDeTeclas.leigth; contador++){
+   const tecla = listaDeTeclas[contador];
+   const instrumento = tecla.classList[1];
+   const idAudio = `#som_ ${instrumento}`;
+   console.log(idAudio);
+   
+   tecla.onclick = function(){
+      tocaSom(idAudio);
    };
-contador = contador + 1;
+   tecla.onkeydown = function(){
+      tecla.classList.add('ativa');
+   }
+   tecla.onkeyup = function(){
+      tecla.classList.remove('ativa');
+   }
+console.log(contador);
 }
