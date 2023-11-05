718b5bfca2a5e01a38ed327f3fd828783101ed13
emelynsouzacezar <128257714+emelynsouzacezar@users.noreply.github.com>
2023-11-05 16:40:52 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
 function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+      const elemento = document.querySelector(idElementoAudio);
+      if(elemento === null){
+         alert('Elemento não encontrado');
+      }
+      if(elemento && elemento.localName === 'audio'){
+         elemento.play();
+      }else {
+         alert('Elemento não encontrado');
+   }
 }
-const listaDeTeclas=document.querySelectorAll('.tecla');
-
-
+const listaDeTeclas = document.querySelectorAll('.tecla');
 //Estrutura de repetição - Enquanto
-for( let contador = 0;  contador < listaDeTeclas.leigth; contador++){
+for( let contador = 0;  contador < listaDeTeclas.length; contador++){
    const tecla = listaDeTeclas[contador];
    const instrumento = tecla.classList[1];
-   const idAudio = `#som_ ${instrumento}`;
+   const idAudio = `#som_${instrumento}`;
+
    console.log(idAudio);
    
    tecla.onclick = function(){
       tocaSom(idAudio);
    };
-   tecla.onkeydown = function(){
+   tecla.onkeydown = function(evento){
+      if(evento.code ==='Enter' || evento.code ==='Space'){
       tecla.classList.add('ativa');
    }
+};
    tecla.onkeyup = function(){
       tecla.classList.remove('ativa');
-   }
+   };
+   console.log(instrumento);
 console.log(contador);
 }
